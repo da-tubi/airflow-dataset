664901ec9d3464f302f376a9facb0ed69d568129
Da Shen <da@tubi.tv>
2023-07-19 16:15:53 +0800

    weekly

--- python/dags/weekly.py
+++ python/dags/weekly.py
@@ -1,18 +1,36 @@
+from __future__ import annotations
+
 import pendulum
 import pprint
 
 from airflow import DAG, Dataset
+from airflow.decorators import task
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
 
-name = "xyz"
+name = "weekly"
 
 ods_dst = Dataset(f"s3://bucket/{name}_ods")
 dwd_dst = Dataset(f"s3://bucket/{name}_dwd")
-dwm_dst = Dataset(f"s3://bucket/{name}_dwm")
+ads_dst = Dataset(f"s3://bucket/{name}_ads")
 
 def print_context(**kwargs):
     pprint.pprint(kwargs)
+
+@task.branch()
+def should_run(**kwargs) -> str:
+    """
+    Determine which empty_task should be run based on if the execution date minute is even or odd.
+
+    :param dict kwargs: Context
+    :return: Id of the task to run
+    """
+    if kwargs["data_interval_start"].day_of_week == 1:
+        return "ads_monday"
+    else:
+        return "ads_skip"
+
 
 with DAG(
     dag_id=f"{name}_ods",
@@ -37,12 +55,15 @@ with DAG(
     )
 
 with DAG(
-    dag_id=f"{name}_dwm",
+    dag_id=f"{name}_ads",
     start_date=pendulum.datetime(2023, 1, 1, tz="UTC"),
     schedule=[dwd_dst],
 ) as dag:
-    BashOperator(
-        task_id="task_dwm",
-        outlets=[dwm_dst],
+    cond = should_run()
+    skip = EmptyOperator(task_id="ads_skip")
+    weekly = BashOperator(
+        task_id="ads_monday",
+        outlets=[ads_dst],
         bash_command="echo {{ data_interval_start | ds }}",
     )
+    cond >> [skip, weekly]
